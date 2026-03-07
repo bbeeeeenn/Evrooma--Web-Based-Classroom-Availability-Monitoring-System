4154633b4c78a15810cddd8bc692e0ae46993001
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-03-07 17:20:11 +0800

    .

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,22 @@ import Image from "next/image";
 export default function Home() {
     return (
         <>
-            <section className="font-inter relative flex h-dvh flex-col items-center pt-40">
-                <h1 className="text-5xl font-bold tracking-widest sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl">
+            <section className="font-inter relative flex h-svh flex-col items-center pt-40">
+                <h1 className="text-5xl font-bold tracking-widest sm:text-7xl lg:text-8xl xl:text-9xl">
                     EVROOMA
                 </h1>
-                <p className="font-inria-sans mt-20 w-[80%] text-center text-3xl">
+                <p className="font-inria-sans mt-10 w-[80%] max-w-2xs text-center text-lg opacity-90 sm:text-xl md:text-2xl lg:text-3xl">
                     <strong>Find</strong> Available Classrooms{" "}
                     <strong>Instantly</strong>
                 </p>
-                <br />
-                <p className="opacity-30">(wait wala pa to)</p>
                 <button
                     onClick={() => (window.location.href = "#roles")}
-                    className="inset-x-0 bottom-0 mt-5 flex cursor-pointer items-center gap-3 rounded-full bg-black px-20 py-3 font-bold tracking-wider text-white"
+                    className="bottom-0 mt-20 flex cursor-pointer items-center gap-3 rounded-full bg-black px-14 py-3 text-sm font-bold tracking-wider text-white sm:px-20"
                 >
-                    Continue <ArrowDown />
+                    Continue <ArrowDown size={20} />
                 </button>
             </section>
-            <section id="roles" className="bg-black-400 relative h-dvh">
+            <section id="roles" className="bg-black-400 relative h-svh">
                 <Image
                     src={"/wave.svg"}
                     alt={""}
