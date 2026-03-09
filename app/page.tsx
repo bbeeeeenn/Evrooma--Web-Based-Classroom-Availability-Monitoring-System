5ba680aa1f207fa7364b409c19c2727b7c66e103
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-03-09 10:44:13 +0800

    REVERT: parallax effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,24 @@
 "use client";
-import clsx from "clsx";
 import { ArrowDown, BookText, GraduationCap, ShieldUser } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-    const title = useRef<HTMLHeadingElement>(null);
-    const description = useRef<HTMLParagraphElement>(null);
-    const btn = useRef<HTMLButtonElement>(null);
-    useEffect(() => {
-        const scrollListener = () => {
-            if (title.current) {
-                title.current.style.marginTop = `${window.scrollY}px`;
-            }
-        };
-        window.addEventListener("scroll", scrollListener);
-        return () => window.removeEventListener("scroll", scrollListener);
-    }, []);
-
     return (
         <>
             <section className="font-inter relative flex h-svh min-h-142.5 flex-col items-center justify-center overflow-hidden">
                 <h1
-                    ref={title}
                     className={
                         "text-5xl font-bold tracking-widest sm:text-7xl lg:text-8xl xl:text-9xl"
                     }
                 >
                     EVROOMA
                 </h1>
-                <p
-                    ref={description}
-                    className="font-inria-sans mt-10 w-[80%] max-w-2xs text-center text-lg opacity-90 sm:text-xl md:text-2xl lg:text-3xl"
-                >
+                <p className="font-inria-sans mt-10 w-[80%] max-w-2xs text-center text-lg opacity-90 sm:text-xl md:text-2xl lg:text-3xl">
                     <strong>Find</strong> Available Classrooms{" "}
                     <strong>Instantly</strong>
                 </p>
                 <button
-                    ref={btn}
                     onClick={() => (window.location.href = "#roles")}
                     className="outline-black-100 bottom-0 mt-20 flex cursor-pointer items-center gap-3 rounded-full bg-black px-14 py-3 text-sm font-bold tracking-wider text-white outline-2 sm:px-20"
                 >
