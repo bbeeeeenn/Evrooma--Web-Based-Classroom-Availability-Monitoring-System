d58b16f8fddc154d68206ed00cce209dcef85f6c
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-03-15 22:13:42 +0800

    .

--- app/(site)/admin/logout/page.tsx
+++ app/(site)/admin/logout/page.tsx
@@ -1,8 +1,14 @@
 import AdminLogout from "./_";
-import { LogoutAdmin } from "@/app/actions/AdminActions";
+import { AuthenticateAdmin, LogoutAdmin } from "@/app/actions/AdminActions";
 import Loading from "../../loading";
+import { redirect } from "next/navigation";
+import { adminLoginPage } from "@/constants";
 
-export default function AdminLogoutPage() {
+export default async function AdminLogoutPage() {
+    if (!(await AuthenticateAdmin())) {
+        redirect(adminLoginPage);
+    }
+
     return (
         <div className="fixed inset-0 m-auto flex size-fit items-center gap-2 text-3xl font-bold">
             <AdminLogout action={LogoutAdmin} />
