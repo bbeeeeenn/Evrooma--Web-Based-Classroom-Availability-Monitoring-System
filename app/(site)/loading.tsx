f2adc543c4e9c2522ca92533d12b2c0f6737aa8b
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-03-15 18:31:40 +0800

    .

--- app/(site)/loading.tsx
+++ app/(site)/loading.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 
 export default function Loading() {
     return (
-        <div className="bg-black-100 dark:bg-black-400 dark:text-black-100 font-inter fixed inset-0 m-auto flex flex-col items-center justify-center gap-3 text-4xl font-semibold tracking-widest">
+        <div className="bg-black-100 dark:bg-black-400 dark:text-black-100 font-inter fixed inset-0 m-auto flex flex-col items-center justify-center gap-3 text-3xl font-semibold tracking-widest">
             <Image
                 src="/favicon_light.svg"
-                width={200}
+                width={150}
                 height={100}
                 alt="logo"
                 className="animate-bounce dark:hidden"
             />
             <Image
                 src="/favicon_dark.svg"
-                width={200}
+                width={150}
                 height={100}
                 alt="logo"
                 className="hidden animate-bounce dark:block"
