b3cee2b870a69e47d8b6baf3134dbc4936f97ed8
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-03-15 21:49:02 +0800

    .

--- app/(site)/admin/layout.tsx
+++ app/(site)/admin/layout.tsx
@@ -1,12 +1,26 @@
 import { AuthenticateAdmin } from "@/app/actions/AdminActions";
 import { AuthProvider } from "@/app/contexts/AuthProvider";
+import React, { Suspense } from "react";
+import Loading from "../loading";
+
+const InitialAuthentication = async ({
+    children,
+}: Readonly<{ children: React.ReactNode }>) => {
+    const initialUser = await AuthenticateAdmin();
+
+    return (
+        <AuthProvider initialUser={initialUser} authAction={AuthenticateAdmin}>
+            {children}
+        </AuthProvider>
+    );
+};
 
 export default async function AdminLayout({
     children,
 }: Readonly<{ children: React.ReactNode }>) {
-    // const initialUser = await AuthenticateAdmin();
-
     return (
-        <AuthProvider authAction={AuthenticateAdmin}>{children}</AuthProvider>
+        <Suspense fallback={<Loading text="Initializing..." />}>
+            <InitialAuthentication>{children}</InitialAuthentication>
+        </Suspense>
     );
 }
